3a9aa54bf78bca8c25ff033b1ce812dbf10e00ff
ornato-t <tommy.ornato@gmail.com>
2022-04-25 17:44:32 +0200

    First commit. Working console.log data flow Prints in the console userID, username and user flair for every comment in PCM

---
+++ index.js
@@ -0,0 +1,24 @@
+const { CommentStream } = require("snoostorm");
+
+require('dotenv').config();
+const Snoowrap = require('snoowrap');
+const Snoostorm = require('snoostorm');
+
+const r = new Snoowrap({
+    userAgent: 'some-description',
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USER,
+    password: process.env.REDDIT_PASS
+});
+
+const stream = new CommentStream(r, {
+    subreddit: 'PoliticalCompassMemes',
+    results: 1
+});
+
+stream.on('item', comment => {
+    let flair = comment.author_flair_text
+    if (flair != null) flair = flair.substring(flair.indexOf('-') + 2)
+    console.log(comment.author_fullname, comment.author.name, flair)
+})
