cbfabf7a75bd9851bba6d76e122f01ccb04ad9f1
ornato-t <tommy.ornato@gmail.com>
2022-04-30 14:04:56 +0200

    Added opt out feature, user agent string, logging Checks for optOut on user document, if found doesn't answer.   For every comment read checks if it's an optOut (command '!cringe'),   if so updates the DB Set user agent string! Added more logging, specifically for optOuts in various points   of the lifecycle

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const uri = process.env.MONGODB_URI
 const client = new MongoClient(uri);
 
 const r = new Snoowrap({
-    userAgent: 'some-description',
+    userAgent: 'flairchange_bot v1.0.0; A bot detecting user flair changes, by u/Nerd02',
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     username: process.env.REDDIT_USER,
@@ -27,6 +27,7 @@ const stream = new CommentStream(r, {
 client.connect()
 const db = client.db('flairChangeBot');
 
+console.log('Starting up...')
 stream.on('item', comment => {
     let flair = comment.author_flair_text
 
@@ -37,28 +38,53 @@ stream.on('item', comment => {
     }
 
     (async() => {
-        await db.collection('PCM_users').findOne({ id: comment.author_fullname }, async(err, res) => { //Check for any already present occurrence
+        db.collection('PCM_users').findOne({ id: comment.author_fullname }, async(err, res) => { //Check for any already present occurrence
             if (err) throw err
 
             if (res === null) { //User not present in DB
-                await db.collection('PCM_users').insertOne({ //Add them
-                    id: comment.author_fullname,
-                    name: comment.author.name,
-                    flair: [flair],
-                    dateAdded: [new Date()]
-                })
+                if (comment.body === '!cringe') { //If user asked for an opt out (as a first message ever on the sub, unlikely)
+                    await db.collection('PCM_users').insertOne({ //Add them + optOut
+                        id: comment.author_fullname,
+                        name: comment.author.name,
+                        flair: [flair],
+                        dateAdded: [new Date()],
+                        optOut: true
+                    })
+                } else {
+                    await db.collection('PCM_users').insertOne({ //Add them
+                        id: comment.author_fullname,
+                        name: comment.author.name,
+                        flair: [flair],
+                        dateAdded: [new Date()]
+                    })
+                }
+
             } else if (res.flair.at(-1) != flair) { //User already present in DB and has update their flair!
-                console.log('Flair change!', comment.author_fullname, comment.author.name, 'was', res.flair.at(-1), 'now is', flair)
+                console.log('Flair change!', comment.author.name, 'was', res.flair.at(-1), 'now is', flair)
 
                 let date = new Date(res.dateAdded.at(-1))
                 let dateStr = date.getUTCFullYear().toString() + '-' + (date.getUTCMonth() + 1).toString() + '-' + date.getUTCDate().toString()
                 let msg = `Did you just change your flair, u/${comment.author.name}? Last time I checked you were **${res.flair.at(-1)}** on ${dateStr}. How come now you are **${flair}**?  \nHave you perhaps shifted your ideals? Because that's cringe, you know?\n\n*"You have the right to change your mind, as I have the right to shame you for doing so." - Anonymus*\n\n^(Bip) ^(bop,) ^(I) ^(am) ^(a) ^(bot.) ^(Don't) ^(get) ^(too) ^(mad.)`
 
-                // comment.reply(msg) //Let's just avoid this. Add on release
+                if (!res.optOut) { //If user did not opt out send message - push to DB either way tho
+                    // comment.reply(msg) //Let's just avoid this. Add on release
+                } else {
+                    console.log('Tried answering but user', comment.author.name, 'opted out')
+                }
 
-                await db.collection('PCM_users').updateOne({ id: comment.author_fullname }, { $push: { flair: flair, dateAdded: new Date() } }, (err, res) => {
+                db.collection('PCM_users').updateOne({ id: comment.author_fullname }, { $push: { flair: flair, dateAdded: new Date() } }, (err, res) => {
                     if (err) throw err
                 })
+
+                if (comment.body === '!cringe') { //If user asked for an opt out (whilst getting called out for changing flair, unlikely)
+                    console.log('Opt-out:', comment.author.name)
+                    await db.collection('PCM_users').updateOne({ id: comment.author_fullname }, { $set: { optOut: true } })
+                }
+            } else { //Default case
+                if (comment.body === '!cringe') { //If user has opted out in generic comment, likely
+                    console.log('Opt-out:', comment.author.name)
+                    await db.collection('PCM_users').updateOne({ id: comment.author_fullname }, { $set: { optOut: true } })
+                }
             }
         })
     })()
--- maintenance/test.js
+++ maintenance/test.js
@@ -6,7 +6,7 @@ const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
 
 const r = new Snoowrap({
-    userAgent: 'some-description',
+    userAgent: 'flairchange_bot v1.0.0; A bot detecting user flair changes, by u/Nerd02',
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     username: process.env.REDDIT_USER,
@@ -21,16 +21,13 @@ const stream = new CommentStream(r, {
 });
 
 stream.on('item', comment => {
-        let user = 'Nerd02',
-            oldFlair = 'QuickBrownFox',
-            newFlair = 'LazyDog',
-            date = new Date(),
-            dateStr = date.getUTCFullYear().toString() + '-' + (date.getUTCMonth() + 1).toString() + '-' + date.getUTCDate().toString()
-
-        let msg = `Did you just change your flair, u/${user}? Last time I checked you were **${oldFlair}** on ${dateStr}. How come now you are **${newFlair}**?  \nHave you perhaps shifted your ideals? Because that's cringe, you know?\n\n*"You have the right to change your mind, as I have the right to shame you for doing so." - Anonymus*\n\n^(Bip) ^(bop,) ^(I) ^(am) ^(a) ^(bot.) ^(Don't) ^(get) ^(too) ^(mad.)`
         if (comment.author_fullname === 't2_105aw2') { //Test line, remove on release (only answers DEV - prevents spam)
-            comment.reply(msg)
             console.log(comment.body)
+            if (comment.body === '!cringe') {
+                console.log('User is cringe')
+                comment.reply('You are cringe')
+            }
+            comment.reply('No cringe detected')
         }
     })
     // console.log(msg)
