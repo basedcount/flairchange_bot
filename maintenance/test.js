c2e992eeaad4dc483617833922a93fb027c6efd4
ornato-t <tommy.ornato@gmail.com>
2022-05-19 14:59:28 +0200

    Adding antispam. Currently set to 1 minute anwers once per minute per user

--- maintenance/test.js
+++ maintenance/test.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGODB_URI
+const client = new MongoClient(uri);
+
 const r = new Snoowrap({
     userAgent: 'flairchange_bot v-TEST; A bot detecting user flair changes, by u/Nerd02',
     clientId: process.env.CLIENT_ID,
@@ -20,13 +24,27 @@ const stream = new CommentStream(r, {
             pollTime: 5000*/ //Add this line if reddit api seems slow
 });
 
+client.connect()
+const db = client.db('flairChangeBot');
+
 const optOutMsg = "You are both cringe and a coward. But fine, let's have it your way. I'll stop calling you out."
 
 console.log('Starting up...')
 stream.on('item', comment => {
     if (comment.author_fullname === 't2_105aw2') {
-        if (comment.body.includes('!cringe')) {
-            comment.reply(optOutMsg)
-        }
+        (async() => {
+            now = new Date()
+            fiveMin = 300000
+            db.collection('PCM_users').findOne({ id: comment.author_fullname }, async(err, res) => { //Check for any already present occurrence
+                if (err) throw err
+
+                if (now.valueOf() > res.dateAdded.at(-1).valueOf() + fiveMin) {
+                    console.log('5 minutes have passed')
+                } else {
+                    console.log('5 minutes haven\'t passed yet')
+                }
+
+            })
+        })()
     }
 })
