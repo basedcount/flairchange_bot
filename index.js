23f6ca0391dd9e190edcfb369718ab8d8336ea33
ornato-t <tommy.ornato@gmail.com>
2022-05-24 00:31:54 +0200

    Adding !flairs command, antispam already included Adding comments Updating messages to report the new command

--- index.js
+++ index.js
@@ -27,8 +27,10 @@ const stream = new CommentStream(r, {
 });
 
 const delay = 10 //delay [minutes] between multiple messages to the same user - prevents spam
-let delayMS = delay * 60000 //same value as above but in milliseconds, needed for JS Date functions
+const delayMS = delay * 60000 //same value as above but in milliseconds, needed for JS Date functions
 const optOutMsg = "You are both cringe and a coward. But fine, let's have it your way. I'll stop calling you out."
+
+let callers = Array() //Array containing the callers who used the "!flairs" command, antispam
 
 client.connect()
 const db = client.db('flairChangeBot');
@@ -71,8 +73,12 @@ stream.on('item', comment => {
         return
     }
 
+    if (comment.body.includes('!flairs') && comment.author_fullname != 't2_mdgp6gdr') { //The bot was summoned using the "!flairs" command
+        summonListFlairsWrapper(comment, db)
+    }
+
     (async() => {
-        let aggEntry
+        let aggEntry //Resulting entry from aggregation pipeline
         await db.collection('PCM_users').aggregate(aggr).forEach(log => { aggEntry = log }) //Running aggregation query for current user - necessary for flair changers ranking
 
         db.collection('PCM_users').findOne({ id: comment.author_fullname }, async(err, res) => { //Check for any already present occurrence
@@ -103,13 +109,13 @@ stream.on('item', comment => {
                 let now = new Date()
                 let date = new Date(res.dateAdded.at(-1))
                 let dateStr = date.getUTCFullYear().toString() + '-' + (date.getUTCMonth() + 1).toString() + '-' + date.getUTCDate().toString() //Composing date using UTC timezone
-                let msg = `Did you just change your flair, u/${comment.author.name}? Last time I checked you were **${res.flair.at(-1)}** on ${dateStr}. How come now you are **${flair}**? Have you perhaps shifted your ideals? Because that's cringe, you know?\n\n*"You have the right to change your mind, as I have the right to shame you for doing so." - Anonymous*\n\n^(Bip bop, I am a bot; don't get too mad. If you want to opt-out write) **^(!cringe)** ^(in a comment)`
+                let msg = `Did you just change your flair, u/${comment.author.name}? Last time I checked you were **${res.flair.at(-1)}** on ${dateStr}. How come now you are **${flair}**? Have you perhaps shifted your ideals? Because that's cringe, you know?\n\n*"You have the right to change your mind, as I have the right to shame you for doing so." - Anonymous*\n\n ^(I am a bot. If you want to opt-out write) **^(!cringe)** ^(in a comment. If you want to check another user's flair history write) **^(!flairs u/<name>)** ^(in a comment.)`
 
                 if (!res.optOut && now.valueOf() > res.dateAdded.at(-1).valueOf() + delayMS) { //If user did not opt out and isn't spamming, send message, push to DB. Doesn't push if user is spamming. SPAM: if bot has written to the same user in the last DELAY minutes
                     if (res.id === aggEntry.id && aggEntry.position <= 10) { //Touch grass message, for multiple flair changers
                         let ratingN = card2ord(aggEntry.position) //Get ordinal number - not for largest
 
-                        msg = `Did you just change your flair, u/${comment.author.name}? Last time I checked you were **${res.flair.at(-1)}** on ${dateStr}. How come now you are **${flair}**?  \nHave you perhaps shifted your ideals? Because that's cringe, you know?\n\nOh and by the way. You have already changed your flair ${aggEntry.size} times, making you the ${ratingN} largest flair changer in this sub.\nGo touch some fucking grass.\n\n*"You have the right to change your mind, as I have the right to shame you for doing so." - Anonymus*\n\n^(Bip bop, I am a bot; don't get too mad. If you want to opt-out write) **^(!cringe)** ^(in a comment)`
+                        let msg = `Did you just change your flair, u/${comment.author.name}? Last time I checked you were **${res.flair.at(-1)}** on ${dateStr}. How come now you are **${flair}**? Have you perhaps shifted your ideals? Because that's cringe, you know?\n\nOh and by the way. You have already changed your flair ${aggEntry.size} times, making you the ${ratingN} largest flair changer in this sub.\nGo touch some fucking grass.\n\n*"You have the right to change your mind, as I have the right to shame you for doing so." - Anonymous*\n\n ^(I am a bot. If you want to opt-out write) **^(!cringe)** ^(in a comment. If you want to check another user's flair history write) **^(!flairs u/<name>)** ^(in a comment.)`
                         console.log('Not a grass toucher', comment.author.name)
                     }
 
@@ -156,6 +162,7 @@ stream.on('item', comment => {
     })()
 })
 
+//Converts a cardinal number(int) to an ordinal one (string), 1 to 10
 function card2ord(param) {
     switch (param) {
         case 1:
@@ -183,6 +190,7 @@ function card2ord(param) {
     }
 }
 
+//Updates the wall of shame. Post ID is hardcoded
 async function wallOfShame(db) {
     let msg = 'This is the wall of shame, containing the names of all the cringe users who opted out using the \`!cringe\` command. May their cowardice never be forgotten.\n\n\n';
 
@@ -199,6 +207,7 @@ async function wallOfShame(db) {
     r.getSubmission('utwvvg').edit(msg) //Update post
 }
 
+//Updates the leaderboard. Post ID is hardcoded
 async function leaderboard(db) {
     let msg = 'This is the leaderboard of the most frequent flair changers of r/PoliticalCompassMemes. If your name appears on this list please turn off your computer and go touch some grass. \n\n'
     const aggr = [{
@@ -229,4 +238,67 @@ async function leaderboard(db) {
     })
     msg += '\n*This post is automatically updated every day at midnight UTC.*'
     await r.getSubmission('uuhlu2').edit(msg) //Update post
+}
+
+//Handles the "!flairs" command, checks wether a user is spamming said command or not, calls summonListFlairs if user isn't spamming
+function summonListFlairsWrapper(comment, db) {
+    let index
+
+    if (callers.find(x => x.id === comment.author_fullname)) { //Is in object...
+        if (callers.find(x => x.date.valueOf() + delayMS < new Date())) { //Is in object and matches criteria
+            console.log('Summon: YES - In object and match criteria', comment.author.name)
+
+            if (summonListFlairs(comment, db)) { //If param is a reddit username, update in the caller array
+                console.log('Updating...')
+                index = callers.findIndex(x => x.id === comment.author_fullname)
+                callers[index].date = new Date()
+            }
+
+        } else { //Is in object and does NOT match criteria
+            console.log('Summon: NO - In object and doesn\'t match criteria', comment.author.name)
+        }
+    } else { //Is not in object, OK
+        console.log('Summon: YES - Not in object', comment.author.name)
+
+        if (summonListFlairs(comment, db)) //If param is a reddit username, push to the caller array
+            callers.push({ id: comment.author_fullname, date: new Date() })
+
+    }
+}
+
+//Composes a message for the flair history of a user
+async function summonListFlairs(comment, db) {
+    const regexReddit = /u\/[A-Za-z0-9_-]+/gm //Regex matching a reddit username:A-Z, a-z, 0-9, _, -
+    const user = comment.body.match(regexReddit) //Extract username 'u/NAME' from the message, according to the REGEX
+    let msg = 'User u/' //Reply message, composed during the function
+
+    if (user == null) { //If no username was provided exit
+        console.log('Tried answering but user', comment.author.name, 'didn\'t enter a reddit username')
+        return false
+    }
+
+    const username = user[0].slice(2) //Cut 'u/', get RAW username
+    msg += username
+
+    log = await db.collection('PCM_users').findOne({ name: username }) //Run query, search for provided username
+    if (log.flair.length > 1) { //Compose the message
+        msg += ` changed their flair ${log.flair.length} times. This makes them unbelievably cringe.`
+    } else {
+        msg += ' never changed their flair.'
+    }
+    msg += ' Here\'s their flair history:\n\n'
+
+    log.flair.forEach((elem, i) => {
+        if (i == 0) {
+            msg += `${i+1}) Started as ${elem} on ${log.dateAdded[i].toUTCString()}.\n\n`
+        } else {
+            msg += `${i+1}) Switched to ${elem} on ${log.dateAdded[i].toUTCString()}.\n\n`
+        }
+    })
+
+    msg += `^(I am a bot, my mission is to spot cringe flair changers. You can check a user's history with the) **^( !flairs u/<name>)** ^(command. Each user can use this command once every ${delay} minutes.)` //Footer
+
+    comment.reply(msg) //Reply!
+
+    return true
 }
--- maintenance/test.js
+++ maintenance/test.js
@@ -24,14 +24,27 @@ const stream = new CommentStream(r, {
         /*,
             pollTime: 5000*/ //Add this line if reddit api seems slow
 });
+
+const delay = 10 //delay [minutes] between multiple messages to the same user - prevents spam
+const delayMS = delay * 60000 //same value as above but in milliseconds, needed for JS Date functions
+
+let callers = Array();
+
 (async() => {
     await client.connect()
     const db = client.db('flairChangeBot');
 
-    wallOfShame(db) //Updates Wall of shame instantly
-    setTimeout(() => { //Updates leaderboard after 10 seconds, avoids RATELIMIT
-        leaderboard(db)
-    }, 10000)
+    stream.on('item', comment => {
+            // if (comment.body.includes('!flairs')) { //Summoned the bot
+            //     summonListFlairsWrapper(comment, db)
+            // }
+            comment.reply(`Did you just change your flair, u/${comment.author.name}? Last time I checked you were **ASD** on SOMETHING. How come now you are **DSA**? Have you perhaps shifted your ideals? Because that's cringe, you know?\n\n*"You have the right to change your mind, as I have the right to shame you for doing so." - Anonymous*\n\n^(I am a bot. If you want to opt-out write) **^(!cringe)** ^(in a comment. If you want to check another user's flair history write)**^( !flairs u/<name>)**^(in a comment)`)
+            console.log(comment.body)
+        })
+        // wallOfShame(db) //Updates Wall of shame instantly
+        // setTimeout(() => { //Updates leaderboard after 10 seconds, avoids RATELIMIT
+        //     leaderboard(db)
+        // }, 10000)
 })()
 
 async function wallOfShame(db) {
@@ -80,4 +93,68 @@ async function leaderboard(db) {
     })
     msg += '\n*This post is automatically updated every day at midnight UTC.*'
     await r.getSubmission('uuhlu2').edit(msg) //Update post
+}
+
+async function summonListFlairs(comment, db) {
+    const regexReddit = /u\/[A-Za-z0-9_-]+/gm //Regex matching a reddit username:A-Z, a-z, 0-9, _, -
+    const user = comment.body.match(regexReddit) //Extract username 'u/NAME' from the message, according to the REGEX
+    let msg = 'User ' //Reply message, composed during the function
+
+    if (user == null) { //If no username was provided exit
+        console.log('Tried answering but user', comment.author.name, 'didn\'t enter a reddit username')
+        return false
+    }
+
+    const username = user[0].slice(2) //Cut 'u/', get RAW username
+    msg += username
+
+    log = await db.collection('PCM_users').findOne({ name: username }) //Run query, search for provided username
+    if (log.flair.length > 1) { //Compose the message
+        msg += ` changed their flair ${log.flair.length} times. This makes them unbelievably cringe.`
+    } else {
+        msg += ' never changed their flair.'
+    }
+    msg += ' Here\'s their flair history:\n\n'
+
+    log.flair.forEach((elem, i) => {
+        if (i == 0) {
+            msg += `${i+1}) Started as ${elem} on ${log.dateAdded[i].toUTCString()}.\n\n`
+        } else {
+            msg += `${i+1}) Switched to ${elem} on ${log.dateAdded[i].toUTCString()}.\n\n`
+        }
+    })
+
+    // comment.reply(msg) //Reply!
+    console.log(msg)
+
+    return true
+}
+
+function summonListFlairsWrapper(comment, db) {
+    let index
+
+    console.log(callers)
+
+    if (callers.find(x => x.id === comment.author_fullname)) { //Is in object...
+        if (callers.find(x => x.date.valueOf() + delayMS < new Date())) { //Is in object and matches criteria
+            console.log('YES - In object and match criteria')
+
+            if (summonListFlairs(comment, db)) {
+                console.log('Updating...')
+                index = callers.findIndex(x => x.id === comment.author_fullname)
+                callers[index].date = new Date()
+            }
+
+        } else { //Is in object and does NOT match criteria
+            console.log('NO - In object and doesn\'t match criteria.')
+        }
+    } else { //Is not in object, OK
+        console.log('YES - Not in object')
+
+        if (summonListFlairs(comment, db)) {
+            console.log('Pushing...')
+            callers.push({ id: comment.author_fullname, date: new Date() })
+        }
+
+    }
 }
