04ba5247d23b31701a4e342f5fd5652f80b4366b
ornato-t <tommy.ornato@gmail.com>
2022-05-23 10:12:42 +0200

    Script for DB purge remove record not respecting the antispam rules

--- maintenance/DBclean.js
+++ maintenance/DBclean.js
@@ -14,25 +14,35 @@ let delayMS = delay * 60000; //same value as above but in milliseconds, needed f
     await client.connect()
     const db = client.db('flairChangeBot');
 
-    cursor = db.collection('test').find()
+    cursor = db.collection('PCM_users').find()
     await cursor.forEach(user => {
         prev = 0
         counter = 0
+        let flairs = Array()
+        let dates = Array()
 
         user.dateAdded.forEach((elem, i) => {
             if (elem - prev < delayMS) {
-                console.log(i, prev, elem, elem - prev, (elem - prev) / 1000, user.flair[i])
                 counter++
             } else {
                 prev = elem
+                dates.push(elem)
+                flairs.push(user.flair[i])
             }
         })
 
-
+        db.collection('PCM_users').updateOne({
+            id: user.id
+        }, {
+            $set: {
+                'flair': flairs,
+                'dateAdded': dates
+            }
+        })
 
         if (counter != 0)
             console.log(user.name, 'removed', counter, 'flairs')
 
     })
-
+    console.log('Done!')
 })()
