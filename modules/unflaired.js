87e94659f632024ac701e3e15a673420963a1021
ornato-t <tommy.ornato@gmail.com>
2022-06-11 14:31:52 +0200

    Updating unflaired insults [removed]

--- modules/unflaired.js
+++ modules/unflaired.js
@@ -3,9 +3,14 @@ const responses = [
     "I don't care. No one does. Get a flair right now or get the hell out of my sub.",
     "Flair the fuck up or leave this sub at once.",
     "The only thing more cringe than changing one's flair is not having one. You are cringe.",
-    "If you don't get a flair right now I'll have AuthLeft make you face the wall.",
+    // "If you don't get a flair right now I'll have AuthLeft make you face the wall.",         //[removed] to please my technocratic overlords
     "If I were you I'd flair the fuck up rather quickly, the mob will be here in no time.",
-    "Despite making up only 13% of the population, unflaired make 100% of the cringe in this sub."
+    "Despite making up only 13% of the population, unflaired make 100% of the cringe in this sub.",
+    "For the crime of being unflaired, I hereby condemn you to being downvoted.",
+    "I see no flair next to your name, why are you still talking?",
+    "Get a flair or get going.",
+    "Cringe and unflaired pilled",
+    "Roses are red,  \n violets are blue;  \n flair changing is cringe  \n and so are you."
 ]
 
 module.exports = responses
