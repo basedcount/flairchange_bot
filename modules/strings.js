c38171fa4349ce85f2e775cc3efa5df0bee17dcf
ornato_t <tommy.ornato@gmail.com>
2022-10-08 17:03:23 +0200

    Reduce !flairs cooldown down to 1'

--- modules/strings.js
+++ modules/strings.js
@@ -51,7 +51,8 @@ function getOptOut() {
 
 //Returns a list of flair changes for the matching 'username'
 function getListFlairs(username, log, delay, pills) {
-    let listFooter = ` ^(Each user can use this command once every ${delay} minutes.)`
+    // let listFooter = ` ^(Each user can use this command once every ${delay} minutes.)`   //Use this if DELAY > 1
+    let listFooter = ` ^(Each user can use this command once every minute.)`
 
     //Easter eggs
     if (username === 'flairchange_bot') {
