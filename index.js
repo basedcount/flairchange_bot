05293c7b9bc8287e3dccebefa902d03973129ea1
ornato_t <tommy.ornato@gmail.com>
2022-10-09 22:10:12 +0200

    v2.4.2 - Remove unnecessary connection to the dataBased

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const uri = process.env.MONGODB_URI
 const basedUri = process.env.BASED_URI
 
 const client = new MongoClient(uri)
-const basedClient = new MongoClient(basedUri)
+// const basedClient = new MongoClient(basedUri)
 const r = new Snoowrap({
-    userAgent: 'flairchange_bot v2.4.1; A bot detecting user flair changes, by u/Nerd02',
+    userAgent: 'flairchange_bot v2.4.2; A bot detecting user flair changes, by u/Nerd02',
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     username: process.env.REDDIT_USER,
@@ -34,10 +34,10 @@ run()
 //Main function
 function run() {
     client.connect()
-    basedClient.connect()
+    // basedClient.connect()
 
     const db = client.db('flairChangeBot').collection('users')
-    const based = basedClient.db('dataBased').collection('users')
+    // const based = basedClient.db('dataBased').collection('users')
 
     console.log('Starting up...')
     if (c.DEBUG) console.log('Warning, DEBUG mode is ON')
