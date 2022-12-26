8facffb09ff793537e8edf8475a3401538d14c77
ornato_t <tommy.ornato@gmail.com>
2022-12-26 02:14:00 +0100

    Link leaderboard to basedcount.com

--- modules/strings.js
+++ modules/strings.js
@@ -2,7 +2,7 @@ import c from "./const.js"
 
 const strings = {
     // outro: `\n\n*"You have the right to change your mind, as I have the right to shame you for doing so." - Anonymous*`, //unused
-    footer: `\n\n[FAQ](https://www.reddit.com/user/flairchange_bot/comments/uf7kuy/bip_bop) - [Leaderboard](https://www.reddit.com/user/flairchange_bot/comments/uuhlu2/leaderboard)\n\n^(I am a bot, my mission is to spot cringe flair changers. If you want to check another user's flair history write) **^(!flairs u/<name>)** ^(in a comment.)`,
+    footer: `\n\n[FAQ](https://www.reddit.com/user/flairchange_bot/comments/uf7kuy/bip_bop) - [Leaderboard](https://basedcount.com/leaderboard?q=flairs)\n\n^(I am a bot, my mission is to spot cringe flair changers. If you want to check another user's flair history write) **^(!flairs u/<name>)** ^(in a comment.)`,
     unflairedChangeOutro: `\n\nYou are beyond cringe, you are disgusting and deserving of all the downvotes you are going to get. Repent now and pick a new flair before it's too late.`,
     optOut: `You are both cringe and a coward, however [I no longer offer opt outs](https://www.reddit.com/user/flairchange_bot/comments/v8f90t/about_the_opt_out_feature/?utm_source=share&utm_medium=web2x&context=3).  \nI'll keep bothering you as much as I do with any other user. Sorry, not sorry.`,
     flairsFCBot: `Nothing to see here. Always been AuthCenter, always will. I'm no flair changer.`,
